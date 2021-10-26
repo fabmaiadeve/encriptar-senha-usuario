1db44918401059626a666b5d4e36b3fa185de548
FabianoMaia <fmaiati@gmail.com>
2021-10-26 16:29:15 -0300

    Encriptar senha e requisicao de validar senha

--- src/main/java/fm/espertostech/encriptarsenhausuario/controller/UsuarioController.java
+++ src/main/java/fm/espertostech/encriptarsenhausuario/controller/UsuarioController.java
@@ -1,12 +1,16 @@
 package fm.espertostech.encriptarsenhausuario.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fm.espertostech.encriptarsenhausuario.model.UsuarioModel;
@@ -17,9 +21,12 @@ import fm.espertostech.encriptarsenhausuario.repository.UsuarioRepository;
 public class UsuarioController {
 	
 	private final UsuarioRepository repository;
+	private final PasswordEncoder encoder;
 	
-	public UsuarioController(UsuarioRepository repository) {
+	
+	public UsuarioController(UsuarioRepository repository, PasswordEncoder encoder) {
 		this.repository = repository;
+		this.encoder = encoder;
 	}
 	
 	@GetMapping(value = "/listartodos")
@@ -31,7 +38,25 @@ public class UsuarioController {
 	@PostMapping(value = "/salvar")
 	public ResponseEntity<UsuarioModel> salvar(@RequestBody UsuarioModel usuario) {
 		
+		usuario.setPassword(encoder.encode(usuario.getPassword()));
+		
 		return ResponseEntity.ok(repository.save(usuario));
 	}
+	
+	@GetMapping(value = "/validarSenha")
+	public ResponseEntity<Boolean> validarSenha (@RequestParam String login, @RequestParam String password) {
+		
+		Optional<UsuarioModel> optUsuario = repository.findByLogin(login);
+		
+		if(optUsuario.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+		}
+		
+		UsuarioModel usuario = optUsuario.get();
+		boolean valid = encoder.matches(password, usuario.getPassword());
+		
+		HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;		
+		return ResponseEntity.status(status).body(valid);
+	} 
 
 }
